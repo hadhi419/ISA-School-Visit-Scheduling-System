a43af55b974a49a7194191107040ef5246aec6a4
hadhi419 <dev.hadhi419@gmail.com>
2026-02-05 19:46:24 +0530

    added uploads directory creation code block

--- backend/src/routes/reportRoutes.js
+++ backend/src/routes/reportRoutes.js
@@ -4,7 +4,6 @@ import { upload } from '../middleware/upload.js';
 
 const router = express.Router();
 
-router.post('/submit', submitMonitoringReport);
-//router.post('/submit', upload.array('files', 5), submitMonitoringReport);
+router.post('/submit', upload.array('files', 5), submitMonitoringReport);
 
 export default router;
