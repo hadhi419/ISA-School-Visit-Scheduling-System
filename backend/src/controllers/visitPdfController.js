606f0dafb460e425f97aeb1b0ffd031bab9c7c7d
hadhi419 <dev.hadhi419@gmail.com>
2026-02-06 07:11:30 +0530

    updated pdf generation code

--- backend/src/controllers/visitPdfController.js
+++ backend/src/controllers/visitPdfController.js
@@ -374,11 +374,12 @@ export const generateVisitPdf = async (req, res) => {
     console.log('Using Chrome:', puppeteer.executablePath());
 
     const browser = await puppeteer.launch({
-      executablePath: process.env.RENDER
-        ? puppeteer.executablePath()
-        : undefined,
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+      ],
     });
 
     const page = await browser.newPage();
